55e9a7e7de95e99d2e25e5f1c416baa8461a10d1
akvghost <chandrawansheakv@gmail.com>
2024-07-21 23:29:42 +0530

    suggested Correction done

--- src/main/java/org/akv/two_pointers/CountPairsWhoseSumIsThanTarget.java
+++ src/main/java/org/akv/two_pointers/CountPairsWhoseSumIsThanTarget.java
@@ -41,7 +41,6 @@ public class CountPairsWhoseSumIsThanTarget {
         for (int i =0;i<arr.length ;i++ ) {
             for (int j = i+1 ;j<arr.length ;j++ ) {
                 if(arr[i]+arr[j] < target){
-//                    System.out.println(arr[i]+" "+arr[j]);
                     countPairs++;
                 }
 //                else System.out.println("in else");
