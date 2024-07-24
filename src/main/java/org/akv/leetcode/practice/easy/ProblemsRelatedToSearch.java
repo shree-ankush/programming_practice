b455109958968aaac0cf3e743b6b7758f76b6c9f
akvghost <chandrawansheakv@gmail.com>
2024-07-24 20:45:33 +0530

    project restructured

--- src/main/java/org/akv/leetcode/practice/easy/ProblemsRelatedToSearch.java
+++ src/main/java/org/akv/leetcode/practice/easy/ProblemsRelatedToSearch.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ProblemsRelatedToSearch {
 
 
-    public int findIndexOfTarget(int [] inputArr,int target){
+    public int findIndexOfTarget(int[] inputArr, int target) {
 
         /**
          * Return the index of the target element using binary search
@@ -16,23 +16,21 @@ public class ProblemsRelatedToSearch {
          * @SpaceComplexity O(1)
          */
 
-        if(inputArr.length==0){
+        if (inputArr.length == 0) {
             return -1;
         }
 
         Arrays.sort(inputArr);
-        int left=0,right=inputArr.length-1;
+        int left = 0, right = inputArr.length - 1;
 
-        while (left<=right){
-            int mid = (left+right)/2;
-            if(target== inputArr[mid]){
-                return  mid;
-            }
-            else if (target>inputArr[mid]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == inputArr[mid]) {
+                return mid;
+            } else if (target > inputArr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
 
         }
@@ -40,7 +38,7 @@ public class ProblemsRelatedToSearch {
         return -1;
     }
 
-    public boolean isTargetPresent(int target,int [] inputArr){
+    public boolean isTargetPresent(int target, int[] inputArr) {
 
         /**
          * Return the index of the target element using binary search
@@ -51,24 +49,22 @@ public class ProblemsRelatedToSearch {
          * @SpaceComplexity O(1)
          */
 
-        if(inputArr.length==0){
+        if (inputArr.length == 0) {
             return false;
         }
 
         Arrays.sort(inputArr);
-        int left=0,right=inputArr.length-1;
+        int left = 0, right = inputArr.length - 1;
 
-        while (left<=right){
-            int mid = (left+right)/2;
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
-            if(inputArr[mid]==target){
-                return  true;
-            }
-            else if (target>inputArr[mid]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
+            if (inputArr[mid] == target) {
+                return true;
+            } else if (target > inputArr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
 
         }
@@ -77,7 +73,7 @@ public class ProblemsRelatedToSearch {
     }
 
 
-    public boolean checkIfNumAndDoubleExists(int [] arr){
+    public boolean checkIfNumAndDoubleExists(int[] arr) {
 
         /**
          * Given an array arr of integers, check if there exist two indices i and j such that :
@@ -91,17 +87,17 @@ public class ProblemsRelatedToSearch {
          * @SpaceComplexity O(1)
          */
 
-        int x=0;
+        int x = 0;
 
-        if(arr.length<=1){
+        if (arr.length <= 1) {
             return false;
         }
 
         for (int i = 0; i < arr.length; i++) {
 
-            if( isTargetPresent(arr[i]*2,arr )){
-                System.out.println("found:"+arr[i]+" "+(arr[i]*2));
-                if(x!=i) {
+            if (isTargetPresent(arr[i] * 2, arr)) {
+                System.out.println("found:" + arr[i] + " " + (arr[i] * 2));
+                if (x != i) {
                     x = 0;
                     return true;
                 }
@@ -114,32 +110,30 @@ public class ProblemsRelatedToSearch {
         return false;
     }
 
-    public int searchIndexOfTargetElseReturnInsertIndex(int[] arr,int target){
+    public int searchIndexOfTargetElseReturnInsertIndex(int[] arr, int target) {
 
         /**
          * Return array(which is sorted and have distinct elements) Index of target and if present else return its index
          * @param arr Array of Integers
          * @param target
-         * @TimeComplexity O(n^2)
+         * @TimeComplexity O(n ^ 2)
          * @SpaceComplexity O(n)
          * @TimeTaken : 35Min
          */
         int start = 0;
         boolean isTargetFound = false;
 
-        if(isTargetPresent(target,arr)){
-            return findIndexOfTarget(arr,target);
+        if (isTargetPresent(target, arr)) {
+            return findIndexOfTarget(arr, target);
         }
 
 
-
-        while(start<= arr.length-1){
-           if(arr[start]<=target){
-              start++;
-           }
-           else {
-               break;
-           }
+        while (start <= arr.length - 1) {
+            if (arr[start] <= target) {
+                start++;
+            } else {
+                break;
+            }
 
 
         }
